095456b42c47fb8ca4aff853c63fd793b6cd09db
connecteddale <35458521+connecteddale@users.noreply.github.com>
2026-02-01 17:02:06 +0200

    feat(03-02): request notification permission at app launch
    - Add init() method to OptionCApp with async permission request
    - Non-blocking: fire and forget via Task
    - App continues regardless of permission result

--- Sources/OptionC/OptionCApp.swift
+++ Sources/OptionC/OptionCApp.swift
@@ -4,6 +4,13 @@ import SwiftUI
 struct OptionCApp: App {
     @StateObject private var appState = AppState()
 
+    init() {
+        // Request notification permission on launch (async, non-blocking)
+        Task {
+            await NotificationManager.shared.requestPermission()
+        }
+    }
+
     var body: some Scene {
         MenuBarExtra {
             MenuBarView(appState: appState)
